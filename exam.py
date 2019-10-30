578e1e1ae8014761badd7512664d137bfaf423dd
Felix Suen <felix.suen2000@gmail.com>
2019-10-30 12:15:39 -0400

    exam schedule:

--- exam.py
+++ exam.py
@@ -2,7 +2,6 @@ from uwaterlooapi import UWaterlooAPI
 from pprint import pprint
 import sys
 
-if __name__ == '__main__':
-    uw = UWaterlooAPI(api_key="7d0e829ff82a1aa902f44d6609a1cbb2")
-    exam = uw.course_examschedule(sys.argv[1], sys.argv[2])
-    pprint(exam)
+uw = UWaterlooAPI(api_key="7d0e829ff82a1aa902f44d6609a1cbb2")
+exam = uw.course_examschedule(sys.argv[1], sys.argv[2])
+pprint(exam)
